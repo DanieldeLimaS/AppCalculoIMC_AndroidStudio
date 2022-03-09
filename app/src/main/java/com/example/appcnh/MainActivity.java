14b46679ef4e36e59cf16188ac04f4cef80c0c94
danielsollus <danielsuporteti@outlook.com.br>
2022-03-08 23:54:07 -0300

    atualizaçao da classe principal, e novos comentarios adicionados para facilitar o entendimento

--- app/src/main/java/com/example/appcnh/MainActivity.java
+++ app/src/main/java/com/example/appcnh/MainActivity.java
@@ -1,23 +1,22 @@
 package com.example.appcnh;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.accessibilityservice.AccessibilityService;
-import android.content.Context;
+import android.graphics.Region;
+import android.os.Bundle;
 import android.util.Log;
-import android.view.inputmethod.InputMethodManager;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.view.View;
-import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText edtAltura,edtPeso;
+    EditText edtAltura, edtPeso;
     Button btnCalcular;
     TextView txtResultado;
 
+    //onCreate É ONDE TUDO SERÁ CRIADO NO MOMENTO QUE O APP SER EXECUTADO, SERIA ONDE OS COMPONENTES DEVEM SER INICIALIZADOS
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,56 +25,69 @@ public class MainActivity extends AppCompatActivity {
         configurarBotaoCalcular();
     }
 
-    protected void CarregarComponentes(){
+    //CARREGA OS COMPONENTES EM UMA VARIAVEL DECALRADA NO INICIO DESSA CLASSE
+    protected void CarregarComponentes() {
         edtAltura = findViewById(R.id.edtAltura);
         edtPeso = findViewById(R.id.edtPeso);
         btnCalcular = findViewById(R.id.btnCalcular);
         txtResultado = findViewById(R.id.txtResultado);
     }
 
+    //MÉTODO PRINCIPAL QUE CONTEM TODA A ESTRUTURA DE EVENTO QUE O BOTÃO CALCULAR IRÁ REALIZAR
     protected void configurarBotaoCalcular() {
         btnCalcular.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String resultado = realizaCalculoRetornaMensagem();
-                Log.d("resultado",resultado);
+                Log.d("resultado", resultado);
                 exibiResultado(resultado);
                 limpaCampos();
             }
 
-            protected String realizaCalculoRetornaMensagem(){
-                double peso = Double.parseDouble(edtPeso.getText().toString());
-                double altura = Double.parseDouble(edtAltura.getText().toString());
-                double resultado = peso/(altura*altura);
-                String texto ="Valores inválidos, tente novamente!";
-                if (resultado <16)
+            //REALIZA O CALCULO E RETORNA A MENSAGEM FILTRADA PARA A TELA
+            protected String realizaCalculoRetornaMensagem() {
+                double peso = Double.parseDouble(edtPeso.getText().toString().replace(",", "."));
+                double altura = Double.parseDouble(edtAltura.getText().toString().replace(",", "."));
+                double resultado = peso / (altura * altura);
+                return RetornaMensagemIMC(resultado);
+
+            }
+
+            /*RETORNA A MENSAGEM PARA O USUÁRIO DE ACORDO  COM O RESULTADO*/
+            private String RetornaMensagemIMC(double resultado) {
+                String texto = "Valores inválidos, tente novamente!";
+                if (resultado < 16)
                     texto = "Magreza grave";
-                else if(resultado >=16 && resultado<17)
+                else if (resultado >= 16 && resultado < 17)
                     texto = "Magreza moderada";
-                else if(resultado >=17 && resultado<18.5)
+                else if (resultado >= 17 && resultado < 18.5)
                     texto = "Magreza leve";
-                else if(resultado >=18.5 && resultado<25)
+                else if (resultado >= 18.5 && resultado < 25)
                     texto = "Saudável";
-                else if(resultado >=25 && resultado<30)
+                else if (resultado >= 25 && resultado < 30)
                     texto = "Sobrepeso";
-                else if(resultado >=30 && resultado<35)
+                else if (resultado >= 30 && resultado < 35)
                     texto = "Obesidade Grau I";
-                else if(resultado >=35 && resultado<40)
+                else if (resultado >= 35 && resultado < 40)
                     texto = "Obesidade Grau I";
-                else if(resultado >=40)
+                else if (resultado >= 40)
                     texto = "Obesidade Grau I";
-                else texto="Valores inválidos, tente novamente!";
+                else texto = "Valores inválidos, tente novamente!";
                 return texto;
             }
 
+            //RESPONSÁVEL POR APENAS EXIBIR A MENSAGEM/RESULTADO NA TELA
             protected void exibiResultado(String resultado) {
                 txtResultado.setText(resultado);
             }
 
+            //LIMPA OS CAMPOS DE PESO E ALTURA APOS APRESENTAR O RESULTADO NA TELA
             protected void limpaCampos() {
                 edtPeso.setText("");
-                edtAltura.requestFocus();
+                edtAltura.setText("");
+                edtPeso.requestFocus();
             }
 
-        });}
+        });
+    }
 }
